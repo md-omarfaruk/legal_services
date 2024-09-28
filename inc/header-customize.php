ec03ef7d621dd73ee9467ae51d547bb88c8f148d
Md. Omar Faruk <87226097+md-omarfaruk@users.noreply.github.com>
2024-09-28 21:17:45 +0600

    one image is added

--- inc/header-customize.php
+++ inc/header-customize.php
@@ -14,7 +14,7 @@ function legal_services_top_header($wp_customize)
     //Setting
     $wp_customize->add_setting('top_header_logo',  array(
         'type' => 'theme_mod',
-         'default'   => get_template_directory_uri() . '/assets/img/moin-choudhury-logo.webp',
+         'default'   => get_template_directory_uri() . '/assets/img/moin-choudhury-logo.png',
     ));
 
     //Control
